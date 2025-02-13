2201b52e095f31551dcbdf94909d2b53a7481275
devp22 <108387315+devp22@users.noreply.github.com>
2025-02-12 21:19:47 -0500

    slr upgraded

--- supervised learning/regression.py
+++ supervised learning/regression.py
@@ -4,7 +4,7 @@ import sklearn
 import matplotlib.pyplot as plt
 import kagglehub
 from sklearn.linear_model import LinearRegression
-
+from sklearn.model_selection import train_test_split
 
 # Linear regression is a statistical method that is used to predict a continuous dependent variable i.e target variable based on one or more independent variables. This technique assumes a linear relationship between the dependent and independent variables which means the dependent variable changes proportionally with changes in the independent variables.
 
@@ -22,14 +22,14 @@ print(numeric_df)
 # linear regression can be calculated using y = b0 + b1*x where b0 is intercept and b1 is slope
 
 # The coefficient of determination, denoted as 𝑅², tells you which amount of variation in 𝑦 can be explained by the dependence on 𝐱, using the particular regression model. A larger 𝑅² indicates a better fit and means that the model can better explain the variation of the output with different inputs.
-x = np.array(numeric_df['age']).reshape(-1,1)
-y = np.array(numeric_df['charges'])
-model = LinearRegression().fit(x,y)
-# coefficient r2 can be determined using score method
-r_sq = model.score(x,y)
-print(f"R2 = {r_sq}")
-print(f"Slope = {model.coef_}")
-print(f"Intercept = {model.intercept_}")
 
-predict_y = model.predict(np.array(32).reshape(-1,1))
-print(f"predict: {predict_y}")
+x = np.array([1,4,9,10,16,20,25]).reshape(-1,1)
+y = np.array([4,8,18,22,34,50,60])
+X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.3)
+model = LinearRegression()
+model.fit(X_train,Y_train)
+Y_pred = model.predict(X_test)
+plt.scatter(X_test,Y_test,c='Red')
+plt.plot(X_test,Y_pred,c='Blue')
+plt.show()
+print("score: ",model.score(X_test,Y_test))
