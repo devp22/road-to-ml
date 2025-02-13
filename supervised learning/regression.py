213c5778d2601e12e4b26456b92d50a51a23fb76
devp22 <108387315+devp22@users.noreply.github.com>
2025-02-12 22:09:30 -0500

    mulitple linear regression with improved accuracy

--- supervised learning/regression.py
+++ supervised learning/regression.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 import kagglehub
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 # Linear regression is a statistical method that is used to predict a continuous dependent variable i.e target variable based on one or more independent variables. This technique assumes a linear relationship between the dependent and independent variables which means the dependent variable changes proportionally with changes in the independent variables.
 
 df = pd.read_csv('datasets/insurance.csv')
+df["sex"] = [0 if i == "male" else 1 for i in df["sex"]]
+df["smoker"] = [0 if i == "no" else 1 for i in df["smoker"]]
+df["region"] = [0 if i == "southeast" else 1 if i == "southwest" else 2 if i == "northwest" else 3 for i in df["region"]]
 numeric_df = df.select_dtypes(include="number")
-# numeric_df.fillna(numeric_df.mean(),inplace=True)
-numeric_df.dropna(inplace=True)
+numeric_df.fillna(numeric_df.mean(),inplace=True)
+# numeric_df.dropna(inplace=True)
 print(numeric_df)
 
 
@@ -23,13 +26,42 @@ print(numeric_df)
 
 # The coefficient of determination, denoted as 𝑅², tells you which amount of variation in 𝑦 can be explained by the dependence on 𝐱, using the particular regression model. A larger 𝑅² indicates a better fit and means that the model can better explain the variation of the output with different inputs.
 
-x = np.array([1,4,9,10,16,20,25]).reshape(-1,1)
-y = np.array([4,8,18,22,34,50,60])
-X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.3)
+# x = np.array([1,4,9,10,16,20,25]).reshape(-1,1)
+# y = np.array([4,8,18,22,34,50,60])
+# X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.3)
+# model = LinearRegression()
+# model.fit(X_train,Y_train)
+# Y_pred = model.predict(X_test)
+# plt.scatter(X_test,Y_test,c='Red')
+# plt.plot(X_test,Y_pred,c='Blue')
+# plt.show()
+# print("score: ",model.score(X_test,Y_test))
+# mae = mean_absolute_error(y_true=Y_test,y_pred=Y_pred) 
+# #squared True returns MSE value, False returns RMSE value. 
+# mse = mean_squared_error(y_true=Y_test,y_pred=Y_pred) #default=True 
+# rmse = mean_squared_error(y_true=Y_test,y_pred=Y_pred,squared=False) 
+# print("MAE:",mae) 
+# print("MSE:",mse) 
+# print("RMSE:",rmse)
+
+
+# MULTIPLE LINEAR REGRESSION (prediction with multiple features)
+
+X = numeric_df.drop(['charges','children'],axis=1)
+y = numeric_df['charges']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=101)
 model = LinearRegression()
-model.fit(X_train,Y_train)
-Y_pred = model.predict(X_test)
-plt.scatter(X_test,Y_test,c='Red')
-plt.plot(X_test,Y_pred,c='Blue')
-plt.show()
-print("score: ",model.score(X_test,Y_test))
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+# plt.scatter(X_test,y_test,c='Red')
+# plt.plot(X_test,y_pred,c='Blue')
+# plt.show()
+score = model.score(X_test,y_test)
+print("score = ",score)
+mae = mean_absolute_error(y_true=y_test,y_pred=y_pred) 
+mse = mean_squared_error(y_true=y_test,y_pred=y_pred) #default=True 
+rmse = mean_squared_error(y_true=y_test,y_pred=y_pred,squared=False) 
+print("MAE:",mae) 
+print("MSE:",mse) 
+print("RMSE:",rmse)
