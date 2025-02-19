9b67623c21b29b4ae9a2d25d95e0153462773069
devp22 <108387315+devp22@users.noreply.github.com>
2025-02-19 05:02:49 -0500

    Multi-Layered NN added

--- neural network/perceptron.py
+++ neural network/perceptron.py
@@ -1,10 +1,9 @@
 import pandas as pd
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder,StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
-# SINGLE_LAYER PERCEPTRON
+from sklearn.neural_network import MLPClassifier
 
 df = pd.read_csv("datasets/penguins_lter.csv")
 df.drop(columns=['Comments'],inplace=True)
@@ -20,8 +19,26 @@ df = pd.concat([df.drop(columns=cols), encoded_df], axis=1)
 y = df['Sex_MALE']
 X = df.drop(columns=['Sex_MALE','Sex_FEMALE'])
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.05,random_state=99)
+
+# SINGLE-LAYER PERCEPTRON
+ 
 perceptron = Perceptron()
 perceptron.fit(X_train,y_train)
 y_pred = perceptron.predict(X=X_test)
 score = accuracy_score(y_true=y_test,y_pred=y_pred)
-print("Score for neural model is: ",score)
+print("Score of singl-layered neural network is: ",score)
+
+# MULTI-LAYER PERCEPTRON
+
+
+#MLP models are highly sensitive to unscaled input data. Since Perceptron is not affected much by scaling, the improvement in MLP is minimal without proper preprocessing.
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.01,random_state=100)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+mlp = MLPClassifier(hidden_layer_sizes=(30,10,5),activation='logistic',max_iter=99,random_state=99)
+mlp.fit(X=X_train_scaled,y=y_train)
+y_pred = mlp.predict(X=X_test_scaled)
+score = accuracy_score(y_pred=y_pred,y_true=y_test)
+print("Score of multi-layered neural model is: ",score)
